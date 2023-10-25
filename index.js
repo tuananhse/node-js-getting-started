7e2e5d290d57b3f29f2b8610b277addadd33b54d
Sai Harshitha Neela <saiharshitha.neela@heroku.com>
2023-10-26 00:28:34 +0530

    adding config vars

--- index.js
+++ index.js
@@ -31,3 +31,18 @@ function showTimes() {
   return result
 }
 
+
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+
